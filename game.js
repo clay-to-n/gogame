a0c8142ed39bf56f32999d344e825ec9aa2df54f
clay-to-n <cbrand@usc.edu>
2015-01-24 00:34:57 -0800

    You can now click to make a circle.  I call this game Circle Grid.  It's not very fun.

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+var GRID = 20; // Pixels between each rule. Should be even
 var canvas;
 var context;
 
@@ -6,24 +7,31 @@ function initialize() {
   context = canvas.getContext('2d');
   canvas.addEventListener("mousedown" , mouseClicked, false);
 
+  // Initialize an array to keep track of board state
+  var p = new Object();
+  p.x = 0;
+  p.y = 0;
+  
+  
   // Draw the board
   context.lineWidth = 1;
   context.strokeStyle = '#bbb';
   for (m=0; m<19; m++) {
     // Draw vertical grid
     context.beginPath();
-    context.moveTo(m*20, 0);
-    context.lineTo(m*20, 320);
+    context.moveTo(m*GRID, 0);
+    context.lineTo(m*GRID, 320);
     context.stroke();
 
     // Draw horizontal grid
     context.beginPath();
-    context.moveTo(0, m*20);
-    context.lineTo(320, m*20);
+    context.moveTo(0, m*GRID);
+    context.lineTo(320, m*GRID);
     context.stroke();
   }
 }
 
+// Called by mouseClicked
 function getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
   return {
@@ -32,9 +40,21 @@ function getMousePos(canvas, evt) {
   };
 }
 
+// Snaps an x or y coordinate to the grid
+function snap(z) {
+  off = z % GRID;
+  if (off > GRID / 2)
+    z = z +(GRID - off);
+  else
+    z = z - off;
+  return z;
+}
+
 // When clicked, place piece
 function mouseClicked(event) {
   var pos = getMousePos(canvas, event);
   context.fillStyle = "#000000";
-  context.fillRect(pos.x, pos.y, 4, 4);
+  context.beginPath();
+  context.arc(snap(pos.x), snap(pos.y), 4, 0, 2 * Math.PI, false);
+  context.fill();
 }
